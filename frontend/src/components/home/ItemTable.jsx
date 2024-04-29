5162da56d1a243036ed588434d2939d4f4949387
Kylreia <rumbauakeith@gmail.com>
2024-04-29 14:05:00 +0100

    added working sort function

--- frontend/src/components/home/ItemTable.jsx
+++ frontend/src/components/home/ItemTable.jsx
@@ -6,25 +6,65 @@ import { MdOutlineAddBox, MdOutlineDelete } from 'react-icons/md';
 
 const ItemTable = ({ items }) => {
   const [searchTerm, setTerm] = useState('');
+  const [sortConfig, setConfig] = useState(null);
+
   const handleSearch = (event) => {
     setTerm(event.target.value.toLowerCase());
   };
 
-  const filteredItems = items.filter(item =>{
-    return item.name.toLowerCase().includes(searchTerm) ||
-           item.brand.toLowerCase().includes(searchTerm) ||
-           item.price.toString().toLowerCase().includes(searchTerm);
+  const handleSort = (event) => {
+    const val = event.target.value;
+    if (val) {
+      const [field, direction] = val.split('-');
+      setConfig({ field, direction });
+    } else {
+      setConfig(null);
+    }
+  };
+
+  const sortedItems = items.filter(item =>
+    item.name.toLowerCase().includes(searchTerm) ||
+    item.brand.toLowerCase().includes(searchTerm) ||
+    item.price.toString().toLowerCase().includes(searchTerm)
+  ).sort((a, b) => {
+    if(sortConfig) {
+      const isReversed = sortConfig.direction === 'desc' ? 1 : -1;
+      if (sortConfig.field === 'price') {
+        // Numerical sort for prices
+        return (a[sortConfig.field] - b[sortConfig.field]) * isReversed;
+      } else {
+        // String sort for other fields
+        const aField = a[sortConfig.field].toString().toLowerCase();
+        const bField = b[sortConfig.field].toString().toLowerCase();
+        if (aField < bField) return -1 * isReversed;
+        if (aField > bField) return 1 * isReversed;
+      }
+    }
+    return 0;
   });
 
   return (
     <div>
-      <div className='flex justify-start items-center ml-4'>
-        <input type="text"
-          placeholder="Search items..."
-          value={searchTerm}
-          onChange={handleSearch}
-          className="mb-4 p-2 border rounded ">
-        </input>
+      <div className='flex flex-row space-x-4'>
+        <div>
+          <input type="text"
+            placeholder="Search items..."
+            value={searchTerm}
+            onChange={handleSearch}
+            className="mb-4 p-2 border rounded ">
+          </input>
+        </div>
+        <div>
+          <select onChange={handleSort} className='mb-4 p-2 border rounded'>
+            <option value="">None</option>
+            <option value="name-desc">Name (A-Z)</option>
+            <option value="name-asc">Name (Z-A)</option>
+            <option value="brand-desc">Brand (A-Z)</option>
+            <option value="brand-asc">Brand (Z-A)</option>
+            <option value="price-desc">Price Lowest to Highest</option>
+            <option value="price-asc">Price Highest to Lowest</option>
+          </select>
+        </div>
       </div>
       <table className='w-full border-separate border-spacing-2'>
             <thead>
@@ -37,7 +77,7 @@ const ItemTable = ({ items }) => {
               </tr>
             </thead>
             <tbody>
-              {filteredItems.map((item, index) =>(
+              {sortedItems.map((item, index) =>(
                 <tr key={item._id} className='h-8'>
                   <td className='border border-slate-700 rounded-md text-center'>
                     {index + 1}
