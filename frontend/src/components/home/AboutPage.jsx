1128f0cc291f074d71982fc2494ca101655bacb0
Kylreia <rumbauakeith@gmail.com>
2024-04-29 10:31:05 +0100

    added hover animation for back button; added sections in about page

--- frontend/src/components/home/AboutPage.jsx
+++ frontend/src/components/home/AboutPage.jsx
@@ -3,7 +3,26 @@ import BackButton from '../BackButton'
 
 const AboutPage = () => {
   return (
-    <div>AboutPage</div>
+    <div className='p-4'>
+      <div>
+        <BackButton />
+      </div>
+      <div>
+        <h1>About This Page</h1>
+        <div>
+          <h2>How the application works</h2>
+        </div>
+        <div>
+          <h2>Technologies involved in the application</h2>
+        </div>
+        <div>
+          <h2>Weaknesses of this application</h2>
+        </div>
+        <div>
+          <h2>Alternatives to implement the application</h2>
+        </div>
+      </div>
+    </div>
   )
 }
 
